078bd1c7ff5ce059057a44cbd165e78e40aa3043
galaadmartineaux <113911888+galaadmartineaux@users.noreply.github.com>
2022-10-05 14:06:19 +0200

    Clearing for next commit.

--- clicker/src/game.d
+++
@@ -1,4 +0,0 @@
-src/game.o: src/game.c ../playground/externals/include/GLFW/glfw3.h \
- ../playground/playground.h src/game.h src/utils.c src/utils.h src/gui.h \
- src/debug/debug.h src/debug/debug.c \
- ../playground/playground_backend_glfw.h
